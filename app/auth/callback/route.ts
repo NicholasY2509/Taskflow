f83eb0998b270c8b716319be719f793e4e81f2d2
Nicholas Yang <nicholsyoung25@gmail.com>
2025-11-22 23:41:00 +0700

    Reconfiguring User

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,3 +1,4 @@
+import { prisma } from "@/lib/prisma";
 import { createClient } from "@/lib/supabase/server";
 import { NextResponse } from "next/server";
 
@@ -9,20 +10,42 @@ export async function GET(request: Request) {
     next = "/";
   }
 
-  if (code) {
-    const supabase = await createClient();
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
-    if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host");
-      const isLocalEnv = process.env.NODE_ENV === "development";
-      if (isLocalEnv) {
-        return NextResponse.redirect(`${origin}${next}`);
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
-      } else {
-        return NextResponse.redirect(`${origin}${next}`);
-      }
+  if (!code) {
+    return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  }
+
+  const supabase = await createClient();
+  const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+  if (error) {
+    return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  }
+
+  const { data: { user } } = await supabase.auth.getUser();
+  if (user) {
+    const exists = await prisma.user.findUnique({
+      where: { id: user.id }
+    })
+    if (!exists) {
+      await prisma.user.create({
+        data: {
+          id: user.id,
+          email: user.email!,
+          name: user.user_metadata.name ?? "",
+          role: "ADMIN",
+        },
+      });
     }
+  }
+
+  const forwardedHost = request.headers.get("x-forwarded-host");
+  const isLocalEnv = process.env.NODE_ENV === "development";
+  if (isLocalEnv) {
+    return NextResponse.redirect(`${origin}${next}`);
+  } else if (forwardedHost) {
+    return NextResponse.redirect(`https://${forwardedHost}${next}`);
+  } else {
+    return NextResponse.redirect(`${origin}${next}`);
   }
 
   return NextResponse.redirect(`${origin}/auth/auth-code-error`);
